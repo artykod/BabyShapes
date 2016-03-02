93820b2cecda41c4215130237be4bc13dce93dc5
Artur Salyakhov <artykod@gmail.com>
2016-03-03 01:14:41 +0500

    initial.

---
+++ Assets/Scripts/Utils/Debug.cs
@@ -0,0 +1,70 @@
+﻿//#define DISABLE_UNITY_DEBUG_OUTPUT
+
+using UnityEngine;
+
+public static class DebugSettings {
+	public static bool IsDebugEnabled {
+		get {
+#if DISABLE_UNITY_DEBUG_OUTPUT
+			return false;
+#else
+			return true;
+#endif
+		}
+	}
+
+	public static bool ShowOnlyAnalytics {
+		get {
+			return false;
+		}
+	}
+}
+
+#if DISABLE_UNITY_DEBUG_OUTPUT
+public class Debug
+{
+	public static bool isDebugBuild
+	{
+		get
+		{
+			return false;
+        }
+	}
+
+	public static void Log(object message)
+	{
+	}
+
+	public static void Log(object message, Object context)
+	{
+	}
+
+	public static void LogError(object message)
+	{
+	}
+
+	public static void LogError(object message, Object context)
+	{
+	}
+
+	public static void LogWarning(object message)
+	{
+	}
+
+	public static void LogWarning(object message, Object context)
+	{
+	}
+
+	public static void LogException(System.Exception e)
+	{
+	}
+
+	public static void LogException(System.Exception e, Object context)
+	{
+	}
+
+	public static void Break()
+	{
+	}
+}
+#endif
---
+++ Assets/Scripts/Utils/SpriteSplitAlphaFixer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class SpriteSplitAlphaFixer : MonoBehaviour {
+#if UNITY_ANDROID
+	private static Dictionary<Texture2D, Material> atlasesMaterialsUI = new Dictionary<Texture2D, Material>();
+
+	private void Awake() {
+		var images = GetComponentsInChildren<Image>(true);
+		foreach (var img in images) {
+			try {
+				if (img != null) {
+					img.material = ModifySprite(img.sprite, "Custom/UISplitAlpha", atlasesMaterialsUI) ?? img.material;
+				}
+			} catch {
+			}
+		}
+	}
+
+	private Material ModifySprite(Sprite sprite, string shader, Dictionary<Texture2D, Material> collection) {
+		if (sprite != null) {
+			var alpha = sprite.associatedAlphaSplitTexture;
+			if (alpha != null) {
+				var texture = sprite.texture;
+				Material material = null;
+
+				if (collection.ContainsKey(texture)) {
+					material = collection[texture];
+				} else {
+					collection[texture] = material = new Material(Shader.Find(shader));
+					material.SetFloat("_AlphaSplitEnabled", 1f);
+					material.SetTexture("_MainTex", texture);
+					material.SetTexture("_AlphaTex", alpha);
+				}
+
+				return material;
+			}
+		}
+
+		return null;
+	}
+#endif
+}

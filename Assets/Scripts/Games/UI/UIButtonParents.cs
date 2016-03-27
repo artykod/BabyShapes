627782742034869902fe3e2a8f202841d0e388a1
Artur Salyakhov <artykod@gmail.com>
2016-03-27 20:27:06 +0500

    parents button click timeout.

--- Assets/Scripts/Games/UI/UIButtonParents.cs
+++ Assets/Scripts/Games/UI/UIButtonParents.cs
@@ -1,11 +1,35 @@
-﻿public class UIButtonParents : UIButton {
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIButtonParents : UIButton {
+	[SerializeField]
+	private Text hintText = null;
+
 	protected override float HoldTime {
 		get {
-			return 0f;
+			return 1f;
 		}
+	}
+
+	protected override void Awake() {
+		base.Awake();
+
+		hintText.enabled = false;
 	}
 
 	protected override void OnClick() {
 		UIDialogParents.ShowSingle();
+	}
+
+	protected override void OnDown() {
+		base.OnDown();
+
+		hintText.enabled = true;
+	}
+
+	protected override void OnUp() {
+		base.OnUp();
+
+		hintText.enabled = false;
 	}
 }
--- Assets/Scripts/Utils/UIButton.cs
+++ Assets/Scripts/Utils/UIButton.cs
@@ -2,10 +2,20 @@
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public abstract class UIButton : MonoBehaviour, IPointerClickHandler, IPointerDownHandler {
+public abstract class UIButton : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler {
 	private float clickStartTime = 0f;
+	private bool clicked = false;
+	private bool clickHandled = false;
 
 	protected abstract void OnClick();
+
+	protected virtual void OnDown() {
+		// for override
+	}
+
+	protected virtual void OnUp() {
+		// for override
+	}
 
 	protected virtual float HoldTime {
 		get {
@@ -13,17 +23,43 @@ public abstract class UIButton : MonoBehaviour, IPointerClickHandler, IPointerDo
 		}
 	}
 
-	void IPointerClickHandler.OnPointerClick(PointerEventData eventData) {
-		var button = GetComponent<Button>();
-		var canClick = button != null ? button.interactable : true;
+	protected virtual void Awake() {
+		// for override
+	}
 
-		if (canClick && (Time.unscaledTime - clickStartTime) > HoldTime) {
-			OnClick();
-			SoundController.Sound(SoundController.SOUND_BUTTON_CLICK);
+	protected virtual void Update() {
+		if (HoldTime > 0f && clicked && !clickHandled && Time.unscaledTime - clickStartTime > HoldTime) {
+			Click();
+		}
+	}
+
+	private void Click() {
+		if (!clickHandled) {
+			var button = GetComponent<Button>();
+			var canClick = button != null ? button.interactable : true;
+			if (canClick) {
+				clickHandled = true;
+				OnClick();
+				SoundController.Sound(SoundController.SOUND_BUTTON_CLICK);
+			}
+		}
+	}
+
+	void IPointerClickHandler.OnPointerClick(PointerEventData eventData) {
+		if (HoldTime <= 0f) {
+			Click();
 		}
 	}
 
 	void IPointerDownHandler.OnPointerDown(PointerEventData eventData) {
+		clickHandled = false;
+		clicked = true;
 		clickStartTime = Time.unscaledTime;
+		OnDown();
+	}
+
+	void IPointerUpHandler.OnPointerUp(PointerEventData eventData) {
+		clicked = false;
+		OnUp();
 	}
 }

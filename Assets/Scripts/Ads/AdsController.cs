6157e31c18cb12ab1f932009478e0f8f33ced480
Artur Salyakhov <artykod@gmail.com>
2016-03-31 01:10:16 +0500

    ads banner only after second game start.

--- Assets/Scripts/Ads/AdsController.cs
+++ Assets/Scripts/Ads/AdsController.cs
@@ -1,14 +1,28 @@
 ﻿using Heyzap;
+using UnityEngine;
 
 public class AdsController : AbstractSingletonBehaviour<AdsController, AdsController> {
+	private const string PREFS_GAME_STARTS_COUNT = "gmsstrtscnt";
+
 	private bool isAdBannerVisible = false;
+
+	private int GameStartsCount {
+		get {
+			return PlayerPrefs.GetInt(PREFS_GAME_STARTS_COUNT, 0);
+		}
+		set {
+			PlayerPrefs.SetInt(PREFS_GAME_STARTS_COUNT, value);
+			PlayerPrefs.Save();
+		}
+	}
 
 	public bool IsAdBannerVisible {
 		get {
 			return isAdBannerVisible;
 		}
 		set {
-			isAdBannerVisible = value;
+			isAdBannerVisible = value && GameStartsCount > 1 && !PurchasesManager.Instance.IsSomeonePurchaseDone;
+
 			if (isAdBannerVisible) {
 				HZBannerAd.ShowWithOptions(new HZBannerShowOptions {
 					Position = HZBannerShowOptions.POSITION_BOTTOM,
@@ -20,6 +34,8 @@ public class AdsController : AbstractSingletonBehaviour<AdsController, AdsContro
 	}
 
 	private void Awake() {
+		GameStartsCount++;
+
 		//HeyzapAds.ShowDebugLogs();
 		//HeyzapAds.ShowThirdPartyDebugLogs();
 		HeyzapAds.Start("ece2341e0547e4d11662e8aadfb64ec1", HeyzapAds.FLAG_NO_OPTIONS);

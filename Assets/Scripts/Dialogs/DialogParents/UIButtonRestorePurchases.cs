dbb51b1ef2f2331179b686937149083fdd145d41
Artur Salyakhov <artykod@gmail.com>
2016-04-03 21:17:49 +0500

    fixes for WSA.

--- Assets/Scripts/Dialogs/DialogParents/UIButtonRestorePurchases.cs
+++ Assets/Scripts/Dialogs/DialogParents/UIButtonRestorePurchases.cs
@@ -1,4 +1,10 @@
 ﻿public class UIButtonRestorePurchases : UIButton {
+	protected override void Awake() {
+#if !UNITY_IOS
+		Destroy(gameObject);
+#endif
+	}
+
 	protected override void OnClick() {
 		PurchasesManager.Instance.RestorePurchases();
 	}
